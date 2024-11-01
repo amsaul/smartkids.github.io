9fa75c7224ee2155ca8bbd55b6fe9af4a4ef0257
amsaul <amsaulbosire@gmail.com>
2024-11-01 07:25:56 +0300

    Update on the login and sign up pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Code } from 'lucide-react';
 
+
 const Header: React.FC = () => {
+  const isActive = (path: string) => {
+    return location.pathname === path ? 'text-yellow-600 font-bold' : 'hover:text-blue-200';
+  };
+  const location = useLocation();
+  // const navigate = useNavigate();
   return (
     <header className="bg-blue-600 text-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
@@ -12,12 +18,12 @@ const Header: React.FC = () => {
         </Link>
         <nav>
           <ul className="flex space-x-6">
-            <li><Link to="/" className="hover:text-blue-200">Home</Link></li>
-            <li><Link to="/courses" className="hover:text-blue-200">Courses</Link></li>
-            <li><Link to="/about" className="hover:text-blue-200">About</Link></li>
-            <li><Link to="/contact" className="hover:text-blue-200">Contact</Link></li>
-            <li><Link to="/login" className="hover:text-blue-200">Login</Link></li>
-            <li><Link to="/register" className="bg-yellow-400 text-blue-800 px-4 py-2 rounded-full hover:bg-yellow-300">Sign Up</Link></li>
+            <li><Link to="/" className={isActive('/')}>Home</Link></li>
+            <li><Link to="/courses" className={isActive('/courses')} >Courses</Link></li>
+            <li><Link to="/about" className={isActive('/about')} >About</Link></li>
+            <li><Link to="/contact" className={isActive('/Contact')}>Contact</Link></li>
+            <li><Link to="/login" className={isActive('/login')}>Join Class</Link></li>
+            <li><Link to="/register" className="bg-yellow-400 text-blue-800 px-4 py-2 rounded-full hover:bg-yellow-300">Register</Link></li>
           </ul>
         </nav>
       </div>
